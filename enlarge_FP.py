0e5adfc2ad9ee3f32d44d569831c08a26df3a00a
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-07-08 19:03:58 +0300

    add enlarge_FP

---
+++ enlarge_FP.py
@@ -0,0 +1,23 @@
+from functools import reduce
+from operator import add
+
+curry = lambda f: lambda x: lambda y: f(x, y)  # noqa: E731
+compose = lambda f: lambda g: lambda x: f(g(x))  # noqa: E731
+
+pair = lambda x: [x, x]  # noqa: E731
+dup = lambda x: x + x  # noqa: E731
+
+image = [
+        '!#',
+        '$@',
+    ]
+# BEGIN (write your solution here)
+concat = curry(reduce)(add)
+concat_map = compose(compose(concat))(curry(map))
+
+enlarge = concat_map(compose(pair)(concat_map(dup)))
+
+# END
+
+print(concat)
+print(concat_map)
