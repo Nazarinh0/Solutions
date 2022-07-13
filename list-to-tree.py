0664e70267696a05c7b4326432143bd82b8b18c6
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-07-13 16:29:34 +0300

    add list-to-tree.py

---
+++ list-to-tree.py
@@ -0,0 +1,16 @@
+def to_item(item):
+    key, value = item
+    return key, convert(value) if isinstance(value, list) else value
+
+
+def convert(tree):
+    result = dict(map(to_item, tree))
+
+    return result
+
+
+
+tree = [
+        ['key4', 'value4'],
+        ['anotherKey', 'vakue']]
+print(convert(tree))
