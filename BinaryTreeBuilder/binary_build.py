19d1757c8b53a98be71d4f0e828f6cfc7c40edef
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-08-03 12:15:14 +0300

    add content to binary_build.py

---
+++ BinaryTreeBuilder/binary_build.py
@@ -0,0 +1,16 @@
+class Node:
+    key = None
+    left = None
+    right = None
+
+    def insert(self, key):
+        if not self.key:
+            self.key = key
+        elif self.key < key:
+            if not self.right:
+                self.right = self.__class__()
+            self.right.insert(key)
+        elif self.key > key:
+            if not self.left:
+                self.left = self.__class__()
+            self.left.insert(key)
