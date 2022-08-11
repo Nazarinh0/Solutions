a1eb9fefcbc5aa41f6f6381b92e99dc69d036fd4
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-08-11 13:20:33 +0300

    add Leetcode dir and 1_two_sum.py

---
+++ Leetcode/1_Two_Sum.py
@@ -0,0 +1,12 @@
+def two_sum(nums, target):
+    result = []
+    for i, num in enumerate(nums):
+        if (target - num) in nums[i+1:]:
+            result.append(nums.index(num))
+            result.append(nums.index((target - num), i + 1))
+            break
+    return result
+
+nums = [2,7,11,15]
+target = 9
+print(two_sum(nums, target))
