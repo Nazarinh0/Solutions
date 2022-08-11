a8bafd196ab973075599d46497048fcd6ef1ab87
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-08-11 13:34:08 +0300

    add reverse_integer.py

---
+++ Leetcode/ReverseInteger/reverse_integer.py
@@ -0,0 +1,11 @@
+def reverse(x):
+    tmp = str(abs(x))
+    result = int(tmp[::-1])
+    if x < 0:
+        result *= -1
+    if result in range(-2 ** 31, 2 ** 31 - 1):
+        return result
+    else:
+        return 0
+
+print(reverse(123))
