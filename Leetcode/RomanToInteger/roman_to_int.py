c90f059c12ae25b0d6f6c2b8873833979318b36f
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-08-16 11:57:49 +0300

    fix in roman_to_int.py

--- Leetcode/RomanToInteger/roman_to_int.py
+++ Leetcode/RomanToInteger/roman_to_int.py
@@ -1,4 +1,4 @@
-def romanToInt(s):
+def roman_to_int(str):
     NUMERALS = {'I': 1,
                 'V': 5,
                 'X': 10,
@@ -8,16 +8,16 @@ def romanToInt(s):
                 'M': 1000}
     result = 0
     i = 0
-    while i <= len(s) - 1:
-        if i == len(s) - 1:
-            result += NUMERALS[s[i]]
+    while i <= len(str) - 1:
+        if i == len(str) - 1:
+            result += NUMERALS[str[i]]
             i += 1
-        elif NUMERALS[s[i]] < NUMERALS[s[i + 1]]:
-            result = result + (NUMERALS[s[i]] * -1)
+        elif NUMERALS[str[i]] < NUMERALS[str[i + 1]]:
+            result = result + (NUMERALS[str[i]] * -1)
             i += 1
         else:
-            result += NUMERALS[s[i]]
+            result += NUMERALS[str[i]]
             i += 1
     return result
 
-print(romanToInt('XX'))
+print(roman_to_int('XX'))
