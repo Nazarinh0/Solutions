630fd6a10d5e77393e89e2209de9178af5e64a47
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-08-22 10:23:59 +0300

    add content to remove_duplicates.py

---
+++ Leetcode/RemoveDuplicates/remove_duplicates.py
@@ -0,0 +1,10 @@
+def removeDuplicates(self, nums: List[int]) -> int:
+    if len(nums) == 0:
+        return 0
+    i = 0
+    for j in range(len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+        j += 1
+    return i + 1
