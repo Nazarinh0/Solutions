5f2d5bc34904bcec8c3d9a563dcb7acbaa4d4a0e
Nazarinh0 <105105599+Nazarinh0@users.noreply.github.com>
2022-08-25 16:45:59 +0300

    Update letter_combinations.py

--- Leetcode/LetterCombinationPhoneNumber/letter_combinations.py
+++ Leetcode/LetterCombinationPhoneNumber/letter_combinations.py
@@ -1,6 +1,5 @@
 def letter_combinations(digits):
-    """
-    Given a string containing digits from 2-9 inclusive,
+    """Given a string containing digits from 2-9 inclusive,
     returns all possible letter combinations that the number could represent.
     """
     
