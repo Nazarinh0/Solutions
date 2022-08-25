7cc1f5d6ff161ecc91f596878103ba9aadab72e6
Nazarinh0 <105105599+Nazarinh0@users.noreply.github.com>
2022-08-25 16:43:10 +0300

    Update reg_exp_match.py

--- Leetcode/RegExpMatch/reg_exp_match.py
+++ Leetcode/RegExpMatch/reg_exp_match.py
@@ -2,8 +2,10 @@ import re
 
 
 def is_match(string, pattern):
-    """Check if the given pattern matches input string.
-    The matching should cover the entire input string (not partial)"""
+    """
+    Check if the given pattern matches input string.
+    The matching should cover the entire input string (not partial)
+    """
     if re.fullmatch(pattern, string) is None:
         return False
     else:
