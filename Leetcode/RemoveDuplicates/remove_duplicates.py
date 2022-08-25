217cd835dabcc198a167dfaff23ae2a65958ec1a
Nazarinh0 <105105599+Nazarinh0@users.noreply.github.com>
2022-08-25 16:46:38 +0300

    Update remove_duplicates.py

--- Leetcode/RemoveDuplicates/remove_duplicates.py
+++ Leetcode/RemoveDuplicates/remove_duplicates.py
@@ -3,6 +3,7 @@ def remove_duplicates(nums):
     removes the duplicates in sorted array in-place.
     returns number of slots in array
     """
+    
     if len(nums) == 0:
         return 0
     i = 0
