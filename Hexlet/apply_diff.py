e3e9b01ac54c14a6c1b02e250b36e0400e9ab6c2
Nazarinh0 <105105599+Nazarinh0@users.noreply.github.com>
2022-08-27 12:25:52 +0300

    Update apply_diff.py

--- Hexlet/apply_diff.py
+++ Hexlet/apply_diff.py
@@ -2,6 +2,8 @@ def apply_diff(items, changes):
     items.update(changes.get('add'))
     items.difference_update(changes.get('remove'))
 
+    
 A, B, C, D = 'abcd'
 target = {A, B, C}
+
 print(apply_diff(target, {'remove': {A, B}}))
