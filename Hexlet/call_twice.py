7dd0e8c5bc47c2845c667b59703d8be4e93d54ed
Nazarinh0 <105105599+Nazarinh0@users.noreply.github.com>
2022-08-27 12:26:01 +0300

    Update call_twice.py

--- Hexlet/call_twice.py
+++ Hexlet/call_twice.py
@@ -3,6 +3,7 @@ def call_twice(function, *args, **kwargs):
     second = function(*args, **kwargs)
     return first, second
 
+
 def push_and_count(target, *, value):
     target.append(target)
     print(len(target))
