5c62dbb006e3528829193241aa9a3cb90c32746b
Nazarinh0 <105105599+Nazarinh0@users.noreply.github.com>
2022-09-02 21:26:08 +0300

    Update chunk_continuous.py

--- Hexlet/chunk_continuous.py
+++ Hexlet/chunk_continuous.py
@@ -1,8 +1,11 @@
 def summary_ranges(nums):
+    
     if len(nums) < 2:
         return []
+    
     start_index = 0
     result = []
+    
     for i in range(len(nums)):
         if i + 1 < len(nums) and nums[i] + 1 == nums[i + 1]:
             continue
