b1540117f1419c7f51c6833c17c1c4266f1ca7cb
Nazarinh0 <105105599+Nazarinh0@users.noreply.github.com>
2022-09-14 15:00:20 +0300

    Update find_longest_lenght.py

--- Hexlet/find_longest_lenght.py
+++ Hexlet/find_longest_lenght.py
@@ -11,7 +11,6 @@ def find_longest_length(string):
             substring = substring + ' ' + substring[substring.index(char)+1:] + char
             
     string_list = substring.split()
-    print(max(string_list, key=len))
     return len(max(string_list, key=len))
 
 print(find_longest_length('jabjcdel'))
