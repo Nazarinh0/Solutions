9706e32f3541038de2f983c71d894e6b3dda3660
Nazarinh0 <105105599+Nazarinh0@users.noreply.github.com>
2022-09-19 21:34:24 +0300

    Update 1nf.sql

--- Hexlet/1nf.sql
+++ Hexlet/1nf.sql
@@ -1,1 +1,22 @@
+--noqa: disable=L010
+-- BEGIN (write your solution here)
+CREATE TABLE users (
+    id bigint PRIMARY KEY,
+    first_name varchar(255),
+    created_at timestamp
+);
+
+INSERT INTO users VALUES (1, 'Tom', '2022-05-24');
+
+CREATE TABLE orders(
+    id bigint PRIMARY KEY,
+    user_first_name varchar(255),
+    months int,
+    created_at timestamp
+);
+
+INSERT INTO orders VALUES
+(1, 'Tom', 2, '2022-09-19'),
+(2, 'Tom', 4, '2022-05-25');
+--END
 
