bd75363c63997797fd737c2220469ec8b655a6e5
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-03-22 15:17:02 +0300

    add 2nd solution to same_tree.py

--- Leetcode/SameTree/same_tree.py
+++ Leetcode/SameTree/same_tree.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
 
 def is_same_tree(p, q):
     if not p and not q:
@@ -13,3 +15,26 @@ def is_same_tree(p, q):
     if p.val == q.val:
         return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
     return False
+
+def isSameTree(p, q):
+    def check(p, q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val == q.val:
+            return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+        return False
+
+    deq = deque([(p, q), ])
+    while deq:
+        p, q = deq.popleft()
+        if not check(p, q):
+            return False
+
+        if p:
+            deq.append((p.left, q.left))
+            deq.append((p.right, q.right))
+    return True
+
+
