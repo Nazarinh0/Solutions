1b5e342e4b15c79a4a5c10a4e4382625d165b4e3
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-26 23:36:11 +0300

    add b_tree.py

---
+++ Hexlet/b_tree.py
@@ -0,0 +1,42 @@
+class BTreeNode:
+    def __init__(self, keys):
+        self.leaf = False
+        self.keys = keys
+        self.children = []
+
+
+def build_b_tree(data):
+    root = BTreeNode(data["keys"])
+    root.leaf = data["leaf"]
+    if not data["leaf"]:
+        for child in data["children"]:
+            root.children.append(build_b_tree(child))
+    return root
+
+
+def solution(items, min, max):
+    btree = build_b_tree(items)
+    return find_values_in_range(btree, min, max)
+
+
+def find_values_in_range(node, min, max):
+    values = []
+    keys = node.keys
+    children = node.children
+
+    # Поиск значений в текущем узле
+    for i in range(len(keys)):
+        if keys[i] >= min and keys[i] <= max:
+            values.append(keys[i])
+
+    # Рекурсивный поиск значений в дочерних узлах
+    if not node.leaf:
+        for i in range(len(children)):
+            if i == 0 and keys[0] >= min:
+                values.extend(find_values_in_range(children[i], min, max))
+            elif i == len(children) - 1 and keys[-1] <= max:
+                values.extend(find_values_in_range(children[i], min, max))
+            elif i < len(children) - 1 and (keys[i] >= min and keys[i] <= max):
+                values.extend(find_values_in_range(children[i], min, max))
+
+    return values
