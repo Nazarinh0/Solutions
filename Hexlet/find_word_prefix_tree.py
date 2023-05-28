6dd5d12fcdcb0e3a31fe2dd842e761db04006bb5
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-28 22:23:31 +0300

    add solution to find_word_prefix_tree.py

--- Hexlet/find_word_prefix_tree.py
+++ Hexlet/find_word_prefix_tree.py
@@ -16,3 +16,26 @@ def build_trie(words):
         node.end = True
     return root
 
+
+# SOLUTION №1
+def solution(words, prefix):
+    tree = build_trie(words)
+    return find_words(tree, prefix)
+
+
+def find_words(node, prefix):
+    for char in prefix:
+        if char in node.children:
+            node = node.children[char]
+        else:
+            return []
+    return find_word_from_node(node, prefix)
+
+
+def find_word_from_node(node, prefix):
+    result = []
+    if node.end:
+        result.append(prefix)
+    for char, child_node in node.children.items():
+        result.extend(find_word_from_node(child_node, prefix + char))
+    return sorted(result)
