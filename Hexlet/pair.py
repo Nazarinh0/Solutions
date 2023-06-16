4ba07db2094ea7614803abef03441af1a68256bd
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-06-16 11:33:46 +0300

    add pair.py

---
+++ Hexlet/pair.py
@@ -0,0 +1,11 @@
+def cons(head, tail):
+    return lambda selector: selector(head, tail)
+
+
+# BEGIN (write your solution here)
+def car(pair):
+    return pair(lambda x, y: x)
+
+def cdr(pair):
+    return pair(lambda x, y: y)
+# END
