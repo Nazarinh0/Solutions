e7646d01a0f4bcf807f288a01ae454b3ceb4969d
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-06-18 21:28:15 +0300

    add content to conditions.go

--- Hexlet/golang_basics/conditions.go
+++ Hexlet/golang_basics/conditions.go
@@ -40,3 +40,31 @@ func DomainForLocale(domain, locale string) string {
 
 	return fmt.Sprintf("%s.%s", locale, domain)
 }
+
+// Реализуйте функцию, которая изменяет строку s в зависимости от переданного режима mode. 
+// когда передается mode "dash", нужно заменить все пробелы на дефисы "-"
+// ModifySpaces("hello world", "dash") // hello-world
+// // когда передается mode "underscore", нужно заменить все пробелы на нижние подчеркивания "_"
+// ModifySpaces("hello world", "underscore") // hello_world
+// // когда передается неизвестный или пустой mode, заменяем все пробелы на звездочки "*"
+// ModifySpaces("hello world", "unknown") // hello*world
+// ModifySpaces("hello world", "") // hello*world
+
+// Для замены символов в строке существует функция ReplaceAll(s, old, new string) string из пакета strings:
+// strings.ReplaceAll("hello world!", "world!", "buddy!") // hello buddy!
+// strings.ReplaceAll("one two three", " ", "_") // one_two_three
+
+func ModifySpaces(s, mode string) string {
+	var replacement string
+
+	switch mode {
+	case "dash":
+		replacement = "-"
+	case "underscore":
+		replacement = "_"
+	default:
+		replacement = "*"
+	}
+
+	return strings.ReplaceAll(s, " ", replacement)
+}
