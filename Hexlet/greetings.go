2748afc24834d5e888cbf461e395f75cf85d6ac4
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-06-18 21:00:16 +0300

    add greetings.go

---
+++ Hexlet/greetings.go
@@ -0,0 +1,18 @@
+package pkg
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Реализуйте функцию Greetings(name string) string, которая вернет строку-приветствие. 
+// Например, при передаче имени Иван, возвращается Привет, Иван!. 
+// Учтите, что имя может быть написано в разном регистре и содержать пробелы.
+
+func Greetings(name string) string {
+	name = strings.Trim(name, " ")
+	name = strings.ToLower(name)
+	name = strings.Title(name) //nolint
+
+	return fmt.Sprintf("Привет, %s!", name)
+}
