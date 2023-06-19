4df98f3ba8542ab54dd5dc87d98717d7449e6dbb
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-06-19 19:56:01 +0300

    fix in basics.go

--- Hexlet/golang_basics/basics.go
+++ Hexlet/golang_basics/basics.go
@@ -5,16 +5,16 @@ import "math"
 
 // Converting int to string
 func IntToString(num int) string {
-  return strconv.Itoa(num)
+  	return strconv.Itoa(num)
 }
 
 // Getting minimum of two given number, math.Min takes float64 type
 func MinInt(x, y int) int {
-	res := math.Min(float64(x), float64(y))
-	return int(res)
-  }
+		res := math.Min(float64(x), float64(y))
+		return int(res)
+}
 
 // Checking if id is positive number and text isn't empty
 func IsValid(id int, text string) bool {
-	return id > 0 && text != ""
-  }
+		return id > 0 && text != ""
+}
