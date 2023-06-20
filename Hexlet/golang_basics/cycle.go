d0039bac7536659049f7961bc16eac810590e842
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-06-20 13:29:56 +0300

    add cycle.go

---
+++ Hexlet/golang_basics/cycle.go
@@ -0,0 +1,23 @@
+
+// Реализуйте функцию, которая преобразует каждый элемент слайса strs с помощью функции mapFunc и возвращает новый слайс. 
+// Учтите, что исходный слайс, который передается как strs, не должен измениться в процессе выполнения.
+
+package pkg
+
+// BEGIN (write your solution here)
+func Map1(strs []string, mapFunc func(string) string) []string {
+        result := make([]string, len(strs))
+        for i := 0; i < len(strs); i++ {
+                result[i] = mapFunc(strs[i])
+        }
+        return result
+}
+
+func Map(strs [string], mapFunc func(string) string) []string {
+		mapped := make([]string, len(strs))
+		for i, s := range strs {
+			mapped[i] = mapFunc(s)
+		}
+
+		return mapped
+}
