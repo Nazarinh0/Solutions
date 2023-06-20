b91a5c8f17ea60ee3902e710e7ad6ee34ba70c7a
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-06-20 17:10:51 +0300

    add content to slices.go

--- Hexlet/golang_basics/slices.go
+++ Hexlet/golang_basics/slices.go
@@ -24,3 +24,40 @@ func Remove2(nums [] int, i int) []int {
 		return nums[:len(nums) - 1]
 }
 
+
+
+// Реализуйте функцию, которая создает копию слайса src с длиной maxLen. 
+// Если maxLen равен нулю или отрицательный, то функция возвращает пустой слайс []int{}. 
+// Если maxLen больше длины src, то возвращается полная копия src.
+func IntsCopy1(src []int, maxLen int) []int {
+		cp := []int{}
+		if maxLen <= 0 {
+				return cp
+		}
+
+		if maxLen > len(src) {
+			cp = make([]int, len(src))
+			copy(cp, src)
+		} else {
+			cp = make([]int, maxLen)
+			copy(cp, src)
+		}
+
+		return cp
+}
+
+func IntsCopy2(src []int, maxLen int) []int {
+		if maxLen <= 0 {
+				return []int{}
+		}
+		
+		if maxLen > len(src) {
+			maxLen = len(src)
+		}
+
+		cp := make([]int, maxLen)
+		copy(cp, src)
+
+		return cp
+
+}
