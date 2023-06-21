db0caca8626f8f448ae92d2a612728743332c332
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-06-21 11:57:39 +0300

    add another solution to sorting slices in slices.go

--- Hexlet/golang_basics/slices.go
+++ Hexlet/golang_basics/slices.go
@@ -67,11 +67,11 @@ func IntsCopy2(src []int, maxLen int) []int {
 // Обработка должна происходить in-place, то есть без создания новых слайсов.
 import "sort"
 
-func UniqueSortedUserIDs(userIDs []int64) []int64 {
+func UniqueSortedUserIDs1(userIDs []int64) []int64 {
 	if len(userIDs) < 2 {
 			return userIDs
 	}
-	
+
 	sort.Slice(userIDs, func(i, j int) bool {
 			return userIDs[i] < userIDs[j]
 	})
@@ -85,4 +85,21 @@ func UniqueSortedUserIDs(userIDs []int64) []int64 {
 			userIDs[j] = userIDs[i]
 	}
 	return userIDs[:j+1]
+}
+
+func UniqueSortedUserIDs2(userIDs []int64) []int64 {
+	if len(userIDs) < 2 {
+		return userIDs
+	}
+
+	sort.SliceStable(userIDs, func(i, j int) bool { return userIDs[i] < userIDs[j] })
+	uniqPointer := 0
+	for i := 1; i < len(userIDs); i++ {
+		if userIDs[uniqPointer] != userIDs[i] {
+			uniqPointer++
+			userIDs[uniqPointer] = userIDs[i]
+		}
+	}
+
+	return userIDs[:uniqPointer+1]
 }
