3c18e4b6773779012ca273f00a78014ad6bc5332
Nazar Annakhalov <105105599+Nazarinh0@users.noreply.github.com>
2023-06-29 22:44:11 +0300

    Update cycle.go

--- Hexlet/golang_basics/cycle.go
+++ Hexlet/golang_basics/cycle.go
@@ -6,11 +6,11 @@ package pkg
 
 // BEGIN (write your solution here)
 func Map1(strs []string, mapFunc func(string) string) []string {
-        result := make([]string, len(strs))
-        for i := 0; i < len(strs); i++ {
-                result[i] = mapFunc(strs[i])
-        }
-        return result
+	        result := make([]string, len(strs))
+	        for i := 0; i < len(strs); i++ {
+	                	result[i] = mapFunc(strs[i])
+	        }
+	        return result
 }
 
 
