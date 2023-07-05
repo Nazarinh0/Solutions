78eb863a31fd1b8af644cacb0bba3edd4c6b4f8d
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-07-05 16:07:58 +0300

    codestyle ifx in strings.go

--- Hexlet/golang_basics/strings.go
+++ Hexlet/golang_basics/strings.go
@@ -87,5 +87,7 @@ func LatinLetters2(s string) string {
 // Шаблон возвращаемой строки:
 // Hello! My name is [name]. I'm [age] y.o. And I also have $[money with precision 2] in my wallet right now.
 func GenerateSelfStory(name string, age int, money float64) string {
-		return fmt.Sprintf("Hello! My name is %s. I'm %d y.o. And I also have $%.2f in my wallet right now.", name, age, money)
+		return fmt.Sprintf(
+			"Hello! My name is %s. I'm %d y.o. And I also have $%.2f in my wallet right now.", name, age, money
+		)
 }
