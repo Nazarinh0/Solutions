2422ae94f24bebba52918e18df1e56d429250bd0
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-11-05 16:21:01 +0500

    remove comments in length_of_last_word.py

--- Leetcode/LengthOfLastWord/length_of_last_word.py
+++ Leetcode/LengthOfLastWord/length_of_last_word.py
@@ -1,7 +1,5 @@
 def length_of_last_word(s: str) -> int:
-    # create string without spaces on sides
     new = s.strip()
-    # split new string and return last substring
     subs = new.split(' ')
     print(subs)
     return len(subs[-1])
