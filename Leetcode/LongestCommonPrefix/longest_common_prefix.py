25352e9a00531532af5a4880ea88f42b34242446
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-11-06 19:07:10 +0500

    codestyle fix in longest_common_prefix.py

--- Leetcode/LongestCommonPrefix/longest_common_prefix.py
+++ Leetcode/LongestCommonPrefix/longest_common_prefix.py
@@ -2,12 +2,15 @@ def lcp(strs):
     """returns the longest common prefix of a given strings"""
     
     prefix = ""
+
     if len(strs) == 0:
         return prefix
+
     strs.sort()
     for x, y in zip(strs[0], strs[-1]):
         if x == y:
             prefix += x
         else:
             break
+
     return prefix
